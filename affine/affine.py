155d2147025f8f9b5050e27a17bd9fde00b06345
AJ Futo <futoaj@gmail.com>
2022-08-23 19:41:25 -0400

    fixing more errors

--- affine/affine.py
+++ affine/affine.py
@@ -1,10 +1,10 @@
 '''
 simple affine cipher encrypter/decrypter
 
-python3 shift.py helloworld e    ->  all 26 encryptions of helloworld
-python3 shift.py mjqqtbtwqi d    ->  all 26 decryptions of mjqqtbtwqi
-python3 shift.py helloworld e 5  ->  encrypts helloworld with shift 5
-python3 shift.py mjqqtbtwqi d 5  ->  decrypts mjqqtbtwqi with shift 5
+python3 affine.py helloworld e        ->  all encryptions of helloworld
+python3 affine.py zoffqcqbft d        ->  all decryptions of zoffqcqbft
+python3 affine.py helloworld e 21 8   ->  encrypts helloworld with a=21, b=8
+python3 affine.py zoffqcqbft d 21 8   ->  decrypts zoffqcqbft with a=21, b=8
 '''
 
 import sys
